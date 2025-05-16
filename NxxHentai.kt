32cfb9645bcb673ac195f7dc70dd9da47e943e74
Moaliii <drashdotcom@gmail.com>
2025-05-16 18:04:34 +0300

    Add files via upload

---
+++ NxxHentai.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.animeextension.ar.nxxhentai
+
+import eu.kanade.tachiyomi.multisrc.videosource.VideoSource
+
+class NxxHentai : VideoSource(
+    "NxxHentai",
+    "https://nxxhentai.com",
+    "ar"
+)
